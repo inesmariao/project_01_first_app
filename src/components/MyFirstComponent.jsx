d368949c77810c97b9576d29f0dc4cd3be253f02
inesmariao <inesmao.misena@gmail.com>
2024-06-27 07:58:04 -0500

    my first component

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MyFirstComponent = () => {
+  return (
+    <>
+      <div>MyFirstComponent</div>
+      <div>Hello World, I'm Inés María Oliveros</div>
+      <div>27 de Junio de 2024</div>
+    </>
+  )
+}
